bf149966921770f71ed006fb932da0e859845221
zombierantcasey <mail@pierremackay.com>
2024-01-13 09:47:48 -0500

    add requirements to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup
 
 setup(
     name="easymysql",
-    version="1.0.3",
+    version="1.0.4",
     author="Pierre MacKay",
     author_email="pierre@propdata.com",
+    description="A simple wrapper for mysql-connector-python.",
+    url="https://github.com/zombierantcasey/easy-mysql",
+    packages=["easymysql"],
     install_requires=[
         "mysql-connector-python",
     ],
